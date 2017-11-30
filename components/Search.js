e7999aedf4af7fdacdaf13ccc20ce47262648ce8
Jared Pleva <Prime@Jareds-MacBook-Pro.local>
2017-11-30 15:38:42 -0700

    adds list with search results

--- components/Search.js
+++ components/Search.js
@@ -9,6 +9,7 @@ import {
     Text,
     View,
     Dimensions,
+    Image
 } from 'react-native';
 import {
     SearchBar, Button
@@ -25,12 +26,280 @@ export default class Search extends Component<{}> {
         this.state = {
             data: [],
             text: '',
+            recipes: [
+                {
+                    "healthLabels": [
+                        "Vegan",
+                        "Vegetarian",
+                        "Peanut-Free",
+                        "Tree-Nut-Free",
+                        "Alcohol-Free"
+                    ],
+                    "image": "https://www.edamam.com/web-img/011/011e89b0cbb9c4c20b765bd02b6c8525.jpg",
+                    "ingredients": [
+                        "1 can Coke (12 ounces)",
+                        "1 inch chunk ginger, peeled and thinly sliced",
+                        "1/2 lemon, sliced into rounds"
+                    ],
+                    "label": "Boiled Coke with Ginger and Lemon Recipe",
+                    "preparation": "http://www.seriouseats.com/recipes/2013/04/boiled-coke-ginger-lemon-hong-kong-recipe.html"
+                },
+                {
+                    "healthLabels": [
+                        "Peanut-Free",
+                        "Tree-Nut-Free",
+                        "Alcohol-Free"
+                    ],
+                    "image": "https://www.edamam.com/web-img/3aa/3aa2da863d7316f12a74a47477a567f7.jpg",
+                    "ingredients": [
+                        "2-3 lbs slab of smoked bacon with skin",
+                        "3 Tbs cherry jam",
+                        "1/4 C dried cherries",
+                        "1 vanilla bean split in half lengthwise",
+                        "15 whole cloves",
+                        "2 liter bottle of Coke",
+                        "1 Granny Smith apple peeled and sliced",
+                        "1 small head of cabbage cored and shredded"
+                    ],
+                    "label": "Cherry Coke Braised Bacon",
+                    "preparation": "http://norecipes.com/blog/2008/03/23/cherry-coke-braised-bacon/"
+                },
+                {
+                    "healthLabels": [
+                        "Vegan",
+                        "Vegetarian",
+                        "Peanut-Free",
+                        "Tree-Nut-Free",
+                        "Alcohol-Free"
+                    ],
+                    "image": "https://www.edamam.com/web-img/011/011e89b0cbb9c4c20b765bd02b6c8525.jpg",
+                    "ingredients": [
+                        "1 can Coke (12 ounces)",
+                        "1 inch chunk ginger, peeled and thinly sliced",
+                        "1/2 lemon, sliced into rounds"
+                    ],
+                    "label": "Boiled Coke with Ginger and Lemon Recipe",
+                    "preparation": "http://www.seriouseats.com/recipes/2013/04/boiled-coke-ginger-lemon-hong-kong-recipe.html"
+                },
+                {
+                    "healthLabels": [
+                        "Peanut-Free",
+                        "Tree-Nut-Free",
+                        "Alcohol-Free"
+                    ],
+                    "image": "https://www.edamam.com/web-img/3aa/3aa2da863d7316f12a74a47477a567f7.jpg",
+                    "ingredients": [
+                        "2-3 lbs slab of smoked bacon with skin",
+                        "3 Tbs cherry jam",
+                        "1/4 C dried cherries",
+                        "1 vanilla bean split in half lengthwise",
+                        "15 whole cloves",
+                        "2 liter bottle of Coke",
+                        "1 Granny Smith apple peeled and sliced",
+                        "1 small head of cabbage cored and shredded"
+                    ],
+                    "label": "Cherry Coke Braised Bacon",
+                    "preparation": "http://norecipes.com/blog/2008/03/23/cherry-coke-braised-bacon/"
+                },
+                {
+                    "healthLabels": [
+                        "Vegan",
+                        "Vegetarian",
+                        "Peanut-Free",
+                        "Tree-Nut-Free",
+                        "Alcohol-Free"
+                    ],
+                    "image": "https://www.edamam.com/web-img/011/011e89b0cbb9c4c20b765bd02b6c8525.jpg",
+                    "ingredients": [
+                        "1 can Coke (12 ounces)",
+                        "1 inch chunk ginger, peeled and thinly sliced",
+                        "1/2 lemon, sliced into rounds"
+                    ],
+                    "label": "Boiled Coke with Ginger and Lemon Recipe",
+                    "preparation": "http://www.seriouseats.com/recipes/2013/04/boiled-coke-ginger-lemon-hong-kong-recipe.html"
+                },
+                {
+                    "healthLabels": [
+                        "Peanut-Free",
+                        "Tree-Nut-Free",
+                        "Alcohol-Free"
+                    ],
+                    "image": "https://www.edamam.com/web-img/3aa/3aa2da863d7316f12a74a47477a567f7.jpg",
+                    "ingredients": [
+                        "2-3 lbs slab of smoked bacon with skin",
+                        "3 Tbs cherry jam",
+                        "1/4 C dried cherries",
+                        "1 vanilla bean split in half lengthwise",
+                        "15 whole cloves",
+                        "2 liter bottle of Coke",
+                        "1 Granny Smith apple peeled and sliced",
+                        "1 small head of cabbage cored and shredded"
+                    ],
+                    "label": "Cherry Coke Braised Bacon",
+                    "preparation": "http://norecipes.com/blog/2008/03/23/cherry-coke-braised-bacon/"
+                },
+                {
+                    "healthLabels": [
+                        "Vegan",
+                        "Vegetarian",
+                        "Peanut-Free",
+                        "Tree-Nut-Free",
+                        "Alcohol-Free"
+                    ],
+                    "image": "https://www.edamam.com/web-img/011/011e89b0cbb9c4c20b765bd02b6c8525.jpg",
+                    "ingredients": [
+                        "1 can Coke (12 ounces)",
+                        "1 inch chunk ginger, peeled and thinly sliced",
+                        "1/2 lemon, sliced into rounds"
+                    ],
+                    "label": "Boiled Coke with Ginger and Lemon Recipe",
+                    "preparation": "http://www.seriouseats.com/recipes/2013/04/boiled-coke-ginger-lemon-hong-kong-recipe.html"
+                },
+                {
+                    "healthLabels": [
+                        "Peanut-Free",
+                        "Tree-Nut-Free",
+                        "Alcohol-Free"
+                    ],
+                    "image": "https://www.edamam.com/web-img/3aa/3aa2da863d7316f12a74a47477a567f7.jpg",
+                    "ingredients": [
+                        "2-3 lbs slab of smoked bacon with skin",
+                        "3 Tbs cherry jam",
+                        "1/4 C dried cherries",
+                        "1 vanilla bean split in half lengthwise",
+                        "15 whole cloves",
+                        "2 liter bottle of Coke",
+                        "1 Granny Smith apple peeled and sliced",
+                        "1 small head of cabbage cored and shredded"
+                    ],
+                    "label": "Cherry Coke Braised Bacon",
+                    "preparation": "http://norecipes.com/blog/2008/03/23/cherry-coke-braised-bacon/"
+                },
+                {
+                    "healthLabels": [
+                        "Vegan",
+                        "Vegetarian",
+                        "Peanut-Free",
+                        "Tree-Nut-Free",
+                        "Alcohol-Free"
+                    ],
+                    "image": "https://www.edamam.com/web-img/011/011e89b0cbb9c4c20b765bd02b6c8525.jpg",
+                    "ingredients": [
+                        "1 can Coke (12 ounces)",
+                        "1 inch chunk ginger, peeled and thinly sliced",
+                        "1/2 lemon, sliced into rounds"
+                    ],
+                    "label": "Boiled Coke with Ginger and Lemon Recipe",
+                    "preparation": "http://www.seriouseats.com/recipes/2013/04/boiled-coke-ginger-lemon-hong-kong-recipe.html"
+                },
+                {
+                    "healthLabels": [
+                        "Peanut-Free",
+                        "Tree-Nut-Free",
+                        "Alcohol-Free"
+                    ],
+                    "image": "https://www.edamam.com/web-img/3aa/3aa2da863d7316f12a74a47477a567f7.jpg",
+                    "ingredients": [
+                        "2-3 lbs slab of smoked bacon with skin",
+                        "3 Tbs cherry jam",
+                        "1/4 C dried cherries",
+                        "1 vanilla bean split in half lengthwise",
+                        "15 whole cloves",
+                        "2 liter bottle of Coke",
+                        "1 Granny Smith apple peeled and sliced",
+                        "1 small head of cabbage cored and shredded"
+                    ],
+                    "label": "Cherry Coke Braised Bacon",
+                    "preparation": "http://norecipes.com/blog/2008/03/23/cherry-coke-braised-bacon/"
+                },
+                {
+                    "healthLabels": [
+                        "Vegan",
+                        "Vegetarian",
+                        "Peanut-Free",
+                        "Tree-Nut-Free",
+                        "Alcohol-Free"
+                    ],
+                    "image": "https://www.edamam.com/web-img/011/011e89b0cbb9c4c20b765bd02b6c8525.jpg",
+                    "ingredients": [
+                        "1 can Coke (12 ounces)",
+                        "1 inch chunk ginger, peeled and thinly sliced",
+                        "1/2 lemon, sliced into rounds"
+                    ],
+                    "label": "Boiled Coke with Ginger and Lemon Recipe",
+                    "preparation": "http://www.seriouseats.com/recipes/2013/04/boiled-coke-ginger-lemon-hong-kong-recipe.html"
+                },
+                {
+                    "healthLabels": [
+                        "Peanut-Free",
+                        "Tree-Nut-Free",
+                        "Alcohol-Free"
+                    ],
+                    "image": "https://www.edamam.com/web-img/3aa/3aa2da863d7316f12a74a47477a567f7.jpg",
+                    "ingredients": [
+                        "2-3 lbs slab of smoked bacon with skin",
+                        "3 Tbs cherry jam",
+                        "1/4 C dried cherries",
+                        "1 vanilla bean split in half lengthwise",
+                        "15 whole cloves",
+                        "2 liter bottle of Coke",
+                        "1 Granny Smith apple peeled and sliced",
+                        "1 small head of cabbage cored and shredded"
+                    ],
+                    "label": "Cherry Coke Braised Bacon",
+                    "preparation": "http://norecipes.com/blog/2008/03/23/cherry-coke-braised-bacon/"
+                },
+                {
+                    "healthLabels": [
+                        "Vegan",
+                        "Vegetarian",
+                        "Peanut-Free",
+                        "Tree-Nut-Free",
+                        "Alcohol-Free"
+                    ],
+                    "image": "https://www.edamam.com/web-img/011/011e89b0cbb9c4c20b765bd02b6c8525.jpg",
+                    "ingredients": [
+                        "1 can Coke (12 ounces)",
+                        "1 inch chunk ginger, peeled and thinly sliced",
+                        "1/2 lemon, sliced into rounds"
+                    ],
+                    "label": "Boiled Coke with Ginger and Lemon Recipe",
+                    "preparation": "http://www.seriouseats.com/recipes/2013/04/boiled-coke-ginger-lemon-hong-kong-recipe.html"
+                },
+                {
+                    "healthLabels": [
+                        "Peanut-Free",
+                        "Tree-Nut-Free",
+                        "Alcohol-Free"
+                    ],
+                    "image": "https://www.edamam.com/web-img/3aa/3aa2da863d7316f12a74a47477a567f7.jpg",
+                    "ingredients": [
+                        "2-3 lbs slab of smoked bacon with skin",
+                        "3 Tbs cherry jam",
+                        "1/4 C dried cherries",
+                        "1 vanilla bean split in half lengthwise",
+                        "15 whole cloves",
+                        "2 liter bottle of Coke",
+                        "1 Granny Smith apple peeled and sliced",
+                        "1 small head of cabbage cored and shredded"
+                    ],
+                    "label": "Cherry Coke Braised Bacon",
+                    "preparation": "http://norecipes.com/blog/2008/03/23/cherry-coke-braised-bacon/"
+                }
+            ],
+            label: '',
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2,
+            })
         }
     }
 
 componentDidMount() {
     console.log("Search Component Works")
     console.log(api,app)
+    this.setState({
+    dataSource: this.state.dataSource.cloneWithRows(this.state.recipes),
+    });
 }
 
 render() {
@@ -43,60 +312,94 @@ render() {
           numberOfLines = {4}
           onChangeText={(text) => this.setState({text})}
           value={this.state.text}/>
-        <Text style={styles.welcome}>
-          {this.state.text}
-        </Text>
         <Button
           onPress={() => {
-            fetch(`https://api.edamam.com/search?q=${this.state.text}&app_id=${app}&app_key=${api}&from=0&to=10`)
-              .then((response) => response.json())
-              .then((responseJson) => {
-                let recipes = [];
-                console.log(responseJson);
-                console.log('Query:',responseJson.q)
-                console.log('Hits:',responseJson.hits)
-                for(let i = 0; i < responseJson.hits.length; i++) {
-                    recipes.push(responseJson.hits[i].recipe);
-                }
-                this.setState({
-                    data: recipes
-                }, function() {
-                  // do something with new state
-                  console.log(this.state.data)
-                  let post = this.state.data
-                  for(let i = 0; i < post.length; i++) {
-                    fetch('https://nomnieats.firebaseio.com/.json/', {  
-                        method: 'POST',
-                        headers: {
-                          'Accept': 'application/json',
-                          'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({
-                            healthLabels: post[i].healthLabels,
-                            cautions: post[i].cautions,
-                            ingredients: post[i].ingredientLines,
-                            label: post[i].label
-                        })
-                      })
-                  }
-                });
-              })
-              .catch((error) => {
-                console.error(error);
-              });
+            // fetch(`https://api.edamam.com/search?q=${this.state.text}&app_id=${app}&app_key=${api}&from=0&to=10`)
+            //   .then((response) => response.json())
+            //   .then((responseJson) => {
+            //     let recipes = [];
+            //     for(let i = 0; i < responseJson.hits.length; i++) {
+            //         recipes.push(responseJson.hits[i].recipe);
+            //     }
+            //     this.setState({
+            //         data: recipes
+            //     }, function() {
+            //       // do something with new state
+            //       let post = this.state.data
+            //       for(let i = 0; i < post.length; i++) {
+            //         fetch('https://nomnieats.firebaseio.com/.json/', {  
+            //             method: 'POST',
+            //             headers: {
+            //               'Accept': 'application/json',
+            //               'Content-Type': 'application/json',
+            //             },
+            //             body: JSON.stringify({
+            //                 healthLabels: post[i].healthLabels,
+            //                 cautions: post[i].cautions,
+            //                 ingredients: post[i].ingredientLines,
+            //                 label: post[i].label,
+            //                 image: post[i].image,
+            //                 preparation: post[i].url
+            //             })
+            //           })
+            //           this.state.recipes.push({
+            //             healthLabels: post[i].healthLabels,
+            //             cautions: post[i].cautions,
+            //             ingredients: post[i].ingredientLines,
+            //             label: post[i].label,
+            //             image: post[i].image,
+            //             preparation: post[i].url
+            //           })
+            //         console.log(this.state.recipes)
+            //           this.setState({
+            //             dataSource: this.state.dataSource.cloneWithRows(this.state.recipes),
+            //           });
+            //       }
+            //     });
+            //   })
+            //   .catch((error) => {
+            //     console.error(error);
+            //   });
           }}
           large
           icon={{name: 'cutlery', type: 'font-awesome'}}
           title='Search Recipes'
           buttonStyle={styles.buttonStyle}/>
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRecipe}
+        style={styles.listView}
+      />
       </View>
     );
   }
+  renderRecipe(recipe){
+      return(
+        <View style={styles.container}>
+            <Image
+                source={{url: recipe.image}}
+                style={styles.thumbnail}
+            />
+            <View style={styles.rightContainer}>
+                <Text style={styles.title}>{recipe.label}</Text>
+            </View>
+        </View>
+      );
+  }
 }
 
+
+
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+      },
     welcome: {
-        fontSize: 60,
+        fontSize: 24,
         textAlign: 'center',
         margin: 10,
         color: 'orange',
@@ -104,6 +407,24 @@ const styles = StyleSheet.create({
     buttonStyle: {
         backgroundColor: 'orange',
         position: 'relative',
-        top: 400,
+        top: 20,
+    },
+    rightContainer: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 16,
+        marginBottom: 8,
+        textAlign: 'center',
+    },
+    listView: {
+        paddingTop: 20,
+        backgroundColor: '#F5FCFF',
+        position: 'relative',
+        top: 40,
+    },
+    thumbnail: {
+        width: 53,
+        height: 81,
     },
 });
