1252fdca6363181bfb1f0319fd435a1a6533b917
Jared Pleva <Prime@Jareds-MacBook-Pro.local>
2017-11-29 17:23:41 -0700

    initial commit

---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to NomniEats!
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  welcome: {
+    fontSize: 60,
+    textAlign: 'center',
+    margin: 10,
+    color: 'orange',
+  },
+});
